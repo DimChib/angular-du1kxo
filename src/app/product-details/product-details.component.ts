49969ee21621b092ee737ca8b0c59eed62f4603d
DimChib <chibisov@cesdo.com>
2019-07-07 13:58:35 +0200

    added CartService

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { products } from '../products';
+import { CartService } from '../cart.service';
+
 
 @Component({
   selector: 'app-product-details',
@@ -7,9 +12,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,) { }
 
-  ngOnInit() {
+    product;
+
+    ngOnInit() {
+  this.route.paramMap.subscribe(params => {
+    this.product = products[+params.get('productId')];
+  });
   }
 
 }
